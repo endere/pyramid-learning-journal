49c40d8da55b8d05db35fd8e3337d34e133a1360
Erik <erik.end@gmail.com>
2017-06-02 19:43:17 -0700

    did step4 stuff

--- pyramid_learning_journal/pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -1,15 +1,19 @@
 """Views for learning journal."""
 from pyramid.view import view_config
 from pyramid_learning_journal.data.data import Posts
-from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import (
+    HTTPNotFound,
+    HTTPFound
+    )
 from pyramid_learning_journal.models import Entry
+import datetime
 
 
 @view_config(route_name='home', renderer='../templates/home.jinja2')
 def list_view(request):
     """Open home page with list of entries."""
     session = request.dbsession
-    all_entries = session.query(Entry).all()
+    all_entries = session.query(Entry).order_by(Entry.id.desc()).all()
     return {'page': 'home', "posts": all_entries}
 
 
@@ -27,7 +31,24 @@ def detail_view(request):
 @view_config(route_name='create', renderer='../templates/create.jinja2')
 def create_view(request):
     """Open new entry page."""
-    return {'page': 'create'}
+    if request.method == "POST" and request.POST:
+        if not request.POST['title'] or not request.POST['body']:
+            return {
+                'title': request.POST['title'],
+                'body': request.POST['body']
+            }
+        the_date=datetime.datetime.now()
+        new_entry = Entry(
+            title=request.POST['title'],
+            body=request.POST['body'],
+            creation_date=the_date.strftime('%A, %-d %B, %Y, %-I:%M %P')
+        )
+        print(new_entry)
+        request.dbsession.add(new_entry)
+        return HTTPFound(
+            location=request.route_url('home')
+        )
+    return {}
 
 
 @view_config(route_name='edit', renderer='../templates/edit.jinja2')
@@ -38,6 +59,12 @@ def edit_view(request):
     entry = session.query(Entry).get(the_id)
     if not entry:
         raise HTTPNotFound
-    return{'page': 'edit', 'entry': entry}
+    if request.method == "GET":
+        return{'page': 'edit', 'entry': entry}
+    if request.method == "POST":
+        entry.title = request.POST['title']
+        entry.body = request.POST['body']
+        request.dbsession.flush()
+        return HTTPFound(request.route_url('detail', id=entry.id))
 
-#entry = list(filter(lambda itemL item['id'] == the_id, JOURNAL ENTRIES))
+
