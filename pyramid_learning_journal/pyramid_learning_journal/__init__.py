db5e3f5611ccb018260354233866ed0c67e50037
Erik <erik.end@gmail.com>
2017-06-02 20:53:41 -0700

    edited main so docstring should be able to work

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -1,9 +1,11 @@
 """Instantiate the inclusion."""
 from pyramid.config import Configurator
+import os
 
 
 def main(global_config, **settings):
     """Function returns a Pyramid WSGI application."""
+    settings['sqlalchemy.url'] = os.envioron.get('DATABASE_URL')
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.routes')
