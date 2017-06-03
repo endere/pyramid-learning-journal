baa75856cc6e68c6e682440f5402f7c03b314fd2
Erik <erik.end@gmail.com>
2017-06-02 22:04:20 -0700

    m

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -6,7 +6,7 @@ import psycopg2
 
 def main(global_config, **settings):
     """Function returns a Pyramid WSGI application."""
-    settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
+   # settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
     #conn_string = "host='localhost:5432' dbname='learning_journal' user='erik' password='1234'"
     conn = psycopg2.connect(
         database='learning_journal',
