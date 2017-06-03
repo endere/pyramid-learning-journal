8908cfd069c7f39702bf2d677afb50267edeef9f
Erik <erik.end@gmail.com>
2017-06-02 22:07:45 -0700

    m

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -6,15 +6,9 @@ import psycopg2
 
 def main(global_config, **settings):
     """Function returns a Pyramid WSGI application."""
-   # settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
+    settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
     #conn_string = "host='localhost:5432' dbname='learning_journal' user='erik' password='1234'"
-    conn = psycopg2.connect(
-        database='learning_journal',
-        user='erik',
-        password='1234',
-        host='localhost',
-        port=5432
-    )
+    conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.routes')
@@ -23,5 +17,3 @@ def main(global_config, **settings):
     config.add_static_view(name='static', path='pyramid_learning_journal:static')
     config.scan()
     return config.make_wsgi_app()
-
-
